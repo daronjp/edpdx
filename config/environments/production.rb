0ba97443465b0221622b3e64a2dc1b1fdad0d778
Daron Peugh <daron@simple.com>
2015-12-15 20:23:19 -0800

    s3 prod config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,8 +90,10 @@ Rails.application.configure do
 
   config.paperclip_defaults = {
   :storage => :s3,
+  :url =>':s3_domain_url',
+  :path => '/:class/:attachment/:id_partition/:style/:filename',
   :s3_credentials => {
-    :bucket => ENV['edpdx'],
+    :bucket => ENV['EDPDX'],
     :access_key_id => ENV['ACCESS_KEY_ID'],
     :secret_access_key => ENV['SECRET_ACCESS_KEY']
   }
