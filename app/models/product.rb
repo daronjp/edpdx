b921e903290811ed28e4d82ee4dfa145b9381fb0
Daron Peugh <daron@simple.com>
2015-12-16 20:13:26 -0800

    active record associations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,6 @@
 class Product < ActiveRecord::Base
   has_many :photos
   has_many :cart_items
+  has_many :product_notes
   has_one :product_category
 end
--- app/models/product_note.rb
+++ app/models/product_note.rb
@@ -1,2 +1,3 @@
 class ProductNote < ActiveRecord::Base
+  belongs_to :product
 end
