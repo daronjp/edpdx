913a604ec38feffd9af08badbf4fb532b03ad93c
Daron Peugh <daron@simple.com>
2016-01-27 07:21:59 -0800

    block bot

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,15 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_filter :no_bot
+
   private
+
+  def no_bot
+    if request.remote_ip == '82.80.249.166'
+      redirect_to 'http://www.google.com'
+    end
+  end
 
   def gate_keeper
       if session[:visitor] == 'admin'
