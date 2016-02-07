e740c1c9cc46c31adf621326efca2f0dad7799f5
Daron Peugh <daron@simple.com>
2016-02-07 14:59:44 -0800

    commits

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,6 @@
 class WelcomeController < ApplicationController
   def index
+    @products = Product.joins(:photos).where("quantity > 0 and is_main = 't' and orientation = 'horizontal'")
     unless session[:visitor] == 'admin'
       Galileo.create(:controller => 'welcome',
                        :view => 'index',
