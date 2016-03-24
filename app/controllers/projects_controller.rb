9715952fa33b9c613e03086bbdca1fca14d3d0ed
Daron Peugh <daron@simple.com>
2016-03-23 20:59:43 -0700

    page view tracking

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,21 @@
 class ProjectsController < ApplicationController
+
+  def succulent
+    unless session[:visitor] == 'admin'
+      Galileo.create(:controller => 'project',
+                       :view => 'succulent',
+                       :ip => request.remote_ip,
+                       :object => params[:id])
+    end
+  end
+
+  def index
+    unless session[:visitor] == 'admin'
+      Galileo.create(:controller => 'project',
+                       :view => 'index',
+                       :ip => request.remote_ip,
+                       :object => params[:id])
+    end
+  end
+
 end
